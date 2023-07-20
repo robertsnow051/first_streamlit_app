71cd371f0d2774ed2979335768882073d8d8170a
robertsnow051 <140095763+robertsnow051@users.noreply.github.com>
2023-07-20 13:51:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,12 @@ import streamlit
 
 streamlit.title('My parents new healthy diner')
 
-   
 
 streamlit.header('Breakfast Menu ')
-  streamlit.text('Omega 3 & Blueberry Oatmeal 🥣')
-  streamlit.text('Kale, Spinach & Rocket Smoothie 🥗')
-  streamlit.text('Hard-Boiled Free-Range Egg 🐔')
-  streamlit.text('Avocado Toast 🥑🍞')
+streamlit.text('Omega 3 & Blueberry Oatmeal 🥣')
+streamlit.text('Kale, Spinach & Rocket Smoothie 🥗')
+streamlit.text('Hard-Boiled Free-Range Egg 🐔')
+streamlit.text('Avocado Toast 🥑🍞')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
